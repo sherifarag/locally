52ccee75fa3fabc33bc6a50de9145775462b6033
sherif farag <shrief196@gmail.com>
2025-01-15 14:16:32 +0200

    initialcommit

--- CSE211_Project/pages/order.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE orders (
-    id INT AUTO_INCREMENT PRIMARY KEY,          -- Unique identifier for each order
-    user_id INT NOT NULL,                       -- ID of the user who placed the order
-    total_amount DECIMAL(10, 2) NOT NULL DEFAULT 0.00, -- Total amount of the order
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Timestamp when the order was created
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- Timestamp when the order was last updated
-    FOREIGN KEY (user_id) REFERENCES users(id)  -- Ensures user_id exists in the users table
-);
--- CSE211_Project/pages/product_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE products (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
-    image VARCHAR(255) NOT NULL,
-    category ENUM('men', 'women', 'kids') NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
